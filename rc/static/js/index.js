cf21dba108d682da326e63a237210a2d5b4a9b23
Michiel De Mey <de.mey.michiel@gmail.com>
2014-03-16 23:41:11 -0700

    Added search functionality! Also added a back button to continue browsing the list.

--- rc/static/js/index.js
+++ rc/static/js/index.js
@@ -5,46 +5,61 @@ window.socket = io.connect();
 var $main   = $('#main'),
     $search = $('#search');
 
-$search.keypress(function(e) {
-  if (e.which == 13) {
-    $main.empty();
-    socket.emit('search', $search.val());
-    $search.blur();
-    e.preventDefault();
-  }
-});
-
-socket.on('movie', function(movie){
-
-  var $img = $('<img />').attr('src', movie.bigImage).data(movie);
-
-  $main.append($img);
-
-  $img.on('click', $main, function(){
-    showOverlay($img.data());
-  });
-
-});
-
 // Show the detail overlay
 var $overlay = $('#overlay');
 var $info = $overlay.find('.info');
 
+function bindListeners() {
+  /* Search */
+  $search.keypress(function(e) {
+    if (e.which == 13) {
+      $main.empty();
+      socket.emit('search', $search.val());
+      $search.blur();
+      e.preventDefault();
+    }
+  });
+
+  /* WebSocket events */
+  socket.on('movie', function(movie){
+    var $img = $('<img />').attr('src', movie.bigImage).data(movie);
+
+    $main.append($img);
+
+    $img.on('click', $main, function(){
+      showOverlay($img.data());
+    });
+  });
+}
+
 function showOverlay(movie) {
-    $overlay.css({ 'background': ' rgba(0,0,0,.1) url(' + movie.backdrop + ') no-repeat center center fixed'});
 
-    var info = '<h1>'+movie.title+'</h1>' +
-      '<h3>' + movie.year + ' ● ' + movie.runtime + 'min</h3>' +
+    // Set background image
+    $overlay.css({ 'background': ' rgba(0,0,0,.1) url(' + movie.backdrop + ') no-repeat center center'});
+
+    var info = '<h1>' + movie.title + '</h1>' +
+      '<h3>' + movie.year + ' &bull; ' + movie.runtime + 'min</h3>' +
       '<p>' + movie.synopsis + '</p>' +
-      '<a href="#">WATCH NOW</a>';
+      '<a class="back" href="#">BACK</a><a class="watch-now" href="#">WATCH NOW</a>';
 
-    $('#main').css('overflow', 'hidden');
     $info.html(info);
     $overlay.show();
 
-    $info.find('a').click(function(){
+    $info.find('.watch-now').click(function(){
       socket.emit('play', movie);
     });
+
+    $info.find('.back').click(function() {
+      closeOverlay();
+    });
 }
+
+function closeOverlay() {
+  $overlay.hide();
+  $info.find('.back').off();
+  $info.find('.watch-now').off();
+}
+
+bindListeners();
 
 })();
